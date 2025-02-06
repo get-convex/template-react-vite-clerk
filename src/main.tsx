5085d5efe376dacb6e53711848d18a2cedd78047
Sarah Shader <sarahshader@gmail.com>
2025-02-05 19:16:07 -0800

    Add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App.tsx";
+import { ErrorBoundary } from "./ErrorBoundary.tsx";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <App />
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
+    <ErrorBoundary>
+      <ClerkProvider
+        publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      >
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <App />
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
+    </ErrorBoundary>
   </StrictMode>,
 );
